9a82db0e135373e1d622c215a41226986e97e7f1
adrian-sierra <87449902+adrian-sierra@users.noreply.github.com>
2022-04-14 02:59:43 -0700

    Add files via upload

---
+++ random.js
@@ -0,0 +1,391 @@
+const cards = {
+    "1": {
+        name: "AceOfHearts",
+        value: 1,
+        card: "./img/ace-hearts.jpg"
+    },
+    "2": {
+        name: "TwoOfHearts",
+        value: 2,
+        card: "./img/two-hearts.jpg"
+    },
+    "3": {
+        name: "ThreeOfHearts",
+        value: 3,
+        card: "./img/three-hearts.jpg"
+    },
+    "4": {
+        name: "FourOfHearts",
+        value: 4,
+        card: "./img/four-hearts.jpg"
+    },
+    "5": {
+        name: "FiveOfHearts",
+        value: 5,
+        card: "./img/five-hearts.jpg"
+    },
+    "6": {
+        name: "SixOfHearts",
+        value: 6,
+        card: "./img/six-hearts.jpg"
+    },
+    "7": {
+        name: "SevenOfHearts",
+        value: 7,
+        card: "./img/seven-hearts.jpg"
+    },
+    "8": {
+        name: "EightOfHearts",
+        value: 8,
+        card: "./img/eight-hearts.jpg"
+    },
+    "9": {
+        name: "NineOfHearts",
+        value: 9,
+        card: "./img/nine-hearts.jpg"
+    },
+    "10": {
+        name: "TenOfHearts",
+        value: 10,
+        card: "./img/ten-hearts.jpg"
+    },
+    "11": {
+        name: "JackOfHearts",
+        value: 10,
+        card: "./img/jack-hearts.jpg"
+    },
+    "12": {
+        name: "QueenOfHearts",
+        value: 10,
+        card: "./img/queen-hearts.jpg"
+    },
+    "13": {
+        name: "KingOfHearts",
+        value: 10,
+        card: "./img/king-hearts.jpg"
+    },
+    "14": {
+        name: "AceOfDiamonds",
+        value: 1,
+        card: "./img/ace-diamonds.jpg"
+    },
+    "15": {
+        name: "TwoOfDiamonds",
+        value: 2,
+        card: "./img/two-diamonds.jpg"
+    },
+    "16": {
+        name: "ThreeOfDiamonds",
+        value: 3,
+        card: "./img/three-diamonds.jpg"
+    },
+    "17": {
+        name: "FourOfDiamonds",
+        value: 4,
+        card: "./img/four-diamonds.jpg"
+    },
+    "18": {
+        name: "FiveOfDiamonds",
+        value: 5,
+        card: "./img/five-diamonds.jpg"
+    },
+    "19": {
+        name: "SixOfDiamonds",
+        value: 6,
+        card: "./img/six-diamonds.jpg"
+    },
+    "20": {
+        name: "SevenOfDiamonds",
+        value: 7,
+        card: "./img/seven-diamonds.jpg"
+    },
+    "21": {
+        name: "EightOfDiamonds",
+        value: 8,
+        card: "./img/eight-diamonds.jpg"
+    },
+    "22": {
+        name: "NineOfDiamonds",
+        value: 9,
+        card: "./img/nine-diamonds.jpg"
+    },
+    "23": {
+        name: "TenOfDiamonds",
+        value: 10,
+        card: "./img/ten-diamonds.jpg"
+    },
+    "24": {
+        name: "JackOfDiamonds",
+        value: 10,
+        card: "./img/jack-diamonds.jpg"
+    },
+    "25": {
+        name: "QueenOfDiamonds",
+        value: 10,
+        card: "./img/queen-diamonds.jpg"
+    },
+    "26": {
+        name: "KingOfDiamonds",
+        value: 10,
+        card: "./img/king-diamonds.jpg"
+    },
+    "27": {
+        name: "AceOfClubs",
+        value: 1,
+        card: "./img/ace-clubs.jpg"
+    },
+    "28": {
+        name: "TwoOfClubs",
+        value: 2,
+        card: "./img/two-clubs.jpg"
+    },
+    "29": {
+        name: "ThreeOfClubs",
+        value: 3,
+        card: "./img/three-clubs.jpg"
+    },
+    "30": {
+        name: "FourOfClubs",
+        value: 4,
+        card: "./img/four-clubs.jpg"
+    },
+    "31": {
+        name: "FiveOfClubs",
+        value: 5,
+        card: "./img/five-clubs.jpg"
+    },
+    "32": {
+        name: "SixOfClubs",
+        value: 6,
+        card: "./img/six-clubs.jpg"
+    },
+    "33": {
+        name: "SevenOfClubs",
+        value: 7,
+        card: "./img/seven-clubs.jpg"
+    },
+    "34": {
+        name: "EightOfClubs",
+        value: 8,
+        card: "./img/eight-clubs.jpg"
+    },
+    "35": {
+        name: "NineOfClubs",
+        value: 9,
+        card: "./img/nine-clubs.jpg"
+    },
+    "36": {
+        name: "TenOfClubs",
+        value: 10,
+        card: "./img/ten-clubs.jpg"
+    },
+    "37": {
+        name: "JackOfClubs",
+        value: 10,
+        card: "./img/jack-clubs.jpg"
+    },
+    "38": {
+        name: "QueenOfClubs",
+        value: 10,
+        card: "./img/queen-clubs.jpg"
+    },
+    "39": {
+        name: "KingOfClubs",
+        value: 10,
+        card: "./img/king-clubs.jpg"
+    },
+    "40": {
+        name: "AceOfSpades",
+        value: 1,
+        card: "./img/ace-spades.jpg"
+    },
+    "41": {
+        name: "TwoOfSpades",
+        value: 2,
+        card: "./img/two-spades.jpg"
+    },
+    "42": {
+        name: "ThreeOfSpades",
+        value: 3,
+        card: "./img/three-spades.jpg"
+    },
+    "43": {
+        name: "FourOfSpades",
+        value: 4,
+        card: "./img/four-spades.jpg"
+    },
+    "44": {
+        name: "FiveOfSpades",
+        value: 5,
+        card: "./img/five-spades.jpg"
+    },
+    "45": {
+        name: "SixOfSpades",
+        value: 6,
+        card: "./img/six-spades.jpg"
+    },
+    "46": {
+        name: "SevenOfSpades",
+        value: 7,
+        card: "./img/seven-spades.jpg"
+    },
+    "47": {
+        name: "EightOfSpades",
+        value: 8,
+        card: "./img/eight-spades.jpg"
+    },
+    "48": {
+        name: "NineOfSpades",
+        value: 9,
+        card: "./img/nine-spades.jpg"
+    },
+    "49": {
+        name: "TenOfSpades",
+        value: 10,
+        card: "./img/ten-spades.jpg"
+    },
+    "50": {
+        name: "JackOfSpades",
+        value: 10,
+        card: "./img/jack-spades.jpg"
+    },
+    "51": {
+        name: "QueenOfSpades",
+        value: 10,
+        card: "./img/queen-spades.jpg"
+    },
+    "52": {
+        name: "KingOfSpades",
+        value: 10,
+        card: "./img/king-spades.jpg"
+    }
+}
+
+function getTotal(arr){ // function to get total sum of an array that is passed in
+    let total = 0;
+    for(let i = 0; i < arr.length; i++){
+        total += arr[i];
+    }
+    return total;
+}
+// function that will pick a random card from the "cards" object if it exists
+// if it does not exist, it will simply call itself until it is able to return an object from "cards"
+function randomCard(){
+    let numIndex = Math.floor(Math.random() * 52 + 1).toString(); // must be from 1 to 52 to maintain even probability
+    return cards[numIndex] || randomCard();
+}
+// function that takes in the object from "cards" and goes through entire "cards" object until
+// the index is found and return that index (object)
+function findKey(card){
+    for(let i = 1; i <= 52; i++){
+        if(card == cards[i.toString()]){
+            return i.toString();
+        }
+    }
+}
+function endGame(){ // function to add cleanup after result
+    document.getElementById("player-add-button").remove();
+    document.getElementById("dealer-add-button").remove();     
+}
+function blackJack(card_1, card_2){ // function that will check all posibilites of a blackjack on the first two hands (ace and a face card)
+    if ( (card_1 == "1" || card_1 == "14" || card_1 == "27" || card_1 == "40") && 
+    (card_2 == "11"  || card_2 == "12"  || card_2 == "13"  || card_2 == "24"  || card_2 == "25"  ||
+    card_2 == "26"  || card_2 == "37"  || card_2 == "38"  || card_2 == "39"  || card_2 == "50"  ||
+    card_2 == "51"  || card_2 == "52") ){
+        return true;
+    }
+    else if ( (card_2 == "1" || card_2 == "14" || card_2 == "27" || card_2 == "40") && 
+    (card_1 == "11"  || card_1 == "12"  || card_1 == "13"  || card_1 == "24"  || card_1 == "25"  ||
+    card_1 == "26"  || card_1 == "37"  || card_1 == "38"  || card_1 == "39"  || card_1 == "50"  ||
+    card_1 == "51"  || card_1 == "52") ){
+        return true;
+    }
+    return false
+}
+
+let playerCard_1 = randomCard(); // generate a random card from the "cards" object
+let playerCard_2 = randomCard();
+let key_1 = findKey(playerCard_1); // way to store the key of the random card generated to check for blackjack
+let key_2 = findKey(playerCard_2);
+
+delete cards[findKey(playerCard_1)]; // delete method will simply delete the designated object passed in
+delete cards[findKey(playerCard_2)];
+// set the image source for the player cards to the generated card object's image source (refer back to "cards" object properties)
+document.getElementById("player-card-1").src = playerCard_1.card;
+document.getElementById("player-card-2").src = playerCard_2.card;
+
+// initialize array that will hold the numerical value of the generated player cards (can pass in entire object, but only value
+// is of importance)
+let playerCardNumbers = [];
+playerCardNumbers.push(playerCard_1.value);
+playerCardNumbers.push(playerCard_2.value);
+document.getElementById("player-total").innerHTML += parseInt(getTotal(playerCardNumbers)); // set the paragraph element to total sum of player first two cards
+
+ if(blackJack(key_1, key_2)) { // call function with arguments being set to the two keys from random cards
+    document.getElementById("player-total").innerHTML = "Player Total: " + 21;
+    document.getElementById("result").innerHTML = "Player Wins!";
+    endGame();
+ }
+
+function addCardPlayer(){ // function that is called whenever the add-card-player button is pressed
+    let newPlayerCard = randomCard(); // generate a random new card each time the function is called (button is clicked)
+    delete cards[findKey(newPlayerCard)]; // again delete the card object generated randomly from "cards" using findKey
+    const img = new Image(); // create new img element with designated width
+    playerCardNumbers.push(newPlayerCard.value); // append to the array holding numerical values the new randomly generated card object's numerical value
+    img.src = newPlayerCard.card; // set the image object's source to new random generated card's source property
+    img.id = "player-new-card";  // way of being able to style more easily using id and corresponding css
+    let temp = document.getElementById("player-total"); // rest of this is styling/adding to the html document
+    document.getElementById("player-total").remove();
+    document.getElementById("player-images").appendChild(img);
+    document.getElementById("player-images").appendChild(temp);
+    document.getElementById("player-total").innerHTML = "Player Total: " + parseInt(getTotal(playerCardNumbers));
+    if(getTotal(playerCardNumbers) == 21){
+        document.getElementById("result").innerHTML = "Player Wins!";
+        endGame();
+    } else if(getTotal(playerCardNumbers) > 21){
+        document.getElementById("result").innerHTML = "Dealer Wins!";
+        endGame();
+    }
+}
+
+// exact same as above, but for dealer's cards
+let dealerCard_1 = randomCard();
+let dealerCard_2 = randomCard();
+let key_3 = findKey(dealerCard_1); 
+let key_4 = findKey(dealerCard_2);
+delete cards[findKey(dealerCard_1)];
+delete cards[findKey(dealerCard_2)];
+
+document.getElementById("dealer-card-1").src = dealerCard_1.card;
+document.getElementById("dealer-card-2").src = dealerCard_2.card;
+
+let dealerCardNumbers = [];
+dealerCardNumbers.push(dealerCard_1.value);
+dealerCardNumbers.push(dealerCard_2.value);
+document.getElementById("dealer-total").innerHTML += parseInt(getTotal(dealerCardNumbers));
+
+if(blackJack(key_3, key_4)) {
+    document.getElementById("dealer-total").innerHTML = "Dealer Total: " + 21;
+    document.getElementById("result").innerHTML = "Dealer Wins!";
+    endGame();
+}
+function addCardDealer(){
+    let newDealerCard = randomCard();
+    delete cards[findKey(newDealerCard)];
+    const img = new Image();
+    dealerCardNumbers.push(newDealerCard.value);
+    img.src = newDealerCard.card;
+    img.id = "dealer-new-card";
+    let temp = document.getElementById("dealer-total");
+    document.getElementById("dealer-total").remove();
+    document.getElementById("dealer-images").appendChild(img);
+    document.getElementById("dealer-images").appendChild(temp);
+    document.getElementById("dealer-total").innerHTML = "Dealer Total: " + parseInt(getTotal(dealerCardNumbers));
+    if(getTotal(dealerCardNumbers) == 21){
+        document.getElementById("result").innerHTML = "Dealer Wins!";
+        endGame();
+    } else if(getTotal(dealerCardNumbers) > 21){
+        document.getElementById("result").innerHTML = "Player Wins!";
+        endGame();
+    }
+}
